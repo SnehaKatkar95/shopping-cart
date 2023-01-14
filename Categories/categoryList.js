338d9f6f5e19c66eef2f1a54683a3bbf93ffcc27
SnehaKatkar95 <122259019+SnehaKatkar95@users.noreply.github.com>
2023-01-14 21:10:37 +0530

    categoryList created

--- Categories/categoryList.js
+++ Categories/categoryList.js
@@ -1,12 +1,19 @@
-import mensProductList from '../product/mensProductList.js';
-import womensProductList from '../product/womensProductList.js';
+import mensProductList from '../Product/mensProductList.js';
+import womensProductList from '../Product/womensProductList.js';
 
-class categoryList{
-get categories(){
-  let mensObject = new mensProductList();
-  mensObject = [mensObject.getShorts(), mensObject.getTees()];
+class categoryList {
+  get categories() {
+    let mensObject = new mensProductList();
+    mensObject = [mensObject.getShorts(), mensObject.getTees()];
 
-  let womensObject = new womensProductList();
-  womensObject = [womensObject.getKurtas, ]
+    let womensObject = new womensProductList();
+    womensObject = [womensObject.getKurtas, womensObject.getSarees];
+
+    return {
+      men: mensObject,
+      women: womensObject,
+    };
+  }
 }
-}
+
+export default categoryList;
